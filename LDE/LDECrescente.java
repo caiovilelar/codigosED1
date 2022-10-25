da9d5583aba836d20c4c35a4691b2f2acd22baed
Caio Vilela <caio.2020200519@unicap.br>
2022-10-25 10:53:08 -0300

    alteração

--- LDE/LDECrescente.java
+++ LDE/LDECrescente.java
@@ -18,22 +18,46 @@ public class LDECrescente<T extends Comparable<T>> {
         }
     }
 
-    public void inserir(T valor) { //Insere um novo nó no início da lista
+    public void inserir(T valor) { //Insere um novo nó na lista
         LDENode<T> novo = new LDENode(valor);
-        if (this.isEmpty() == true) {
+        LDENode<T> aux, anterior;
+        if (this.isEmpty() == true) { //Lista vazia?
             this.prim = novo;
             this.ult = novo;
             this.qtd++;
-        } else if (valor.compareTo(this.prim.getInfo()) < 0 ){
+        } else if (valor.compareTo(this.prim.getInfo()) < 0 ){ // Insere no início
             novo.setProx(this.prim);
             this.prim.setAnt(novo);
             this.prim = novo;
             this.qtd++;
-        } else if (valor.compareTo(this.ult.getInfo()) > 0) {
+        } else if (valor.compareTo(this.prim.getInfo()) == 0 ){ // Verifica repetição
+            System.out.println("Valor Repetido. Inserção não efetuada.");
+        } else if (valor.compareTo(this.ult.getInfo()) > 0) { // Insere no final
             this.ult.setProx(novo);
             novo.setAnt(this.ult);
             this.ult = novo;
             this.qtd++;
+        } else if (valor.compareTo(this.ult.getInfo()) == 0 ){ // Verifica repetição
+            System.out.println("Valor Repetido. Inserção não efetuada.");
+        } else { // Insere no meio
+            aux = this.prim.getProx();
+            while (true) {
+                if (valor.compareTo(aux.getInfo()) == 0) { // Verifica repetição
+                    System.out.println("Valor repetido. Inserção não efetuada.");
+                    break;
+                } else if (valor.compareTo(aux.getInfo()) < 0){ // Insere no meio da lista
+                    anterior = aux.getAnt();
+                    anterior.setProx(novo);
+                    aux.setAnt(novo);
+                    novo.setProx(aux);
+                    novo.setAnt(anterior);
+                    this.qtd++;
+                    break;
+                } else {
+                    aux = aux.getProx();
+                }
+
+            }
         }
     }
 }
