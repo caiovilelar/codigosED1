2877341310223cbf99e00e9d67387eefe81cb726
Caio Vilela <caio.2020200519@unicap.br>
2022-11-01 10:22:20 -0300

    alterando o metodo remover

--- LDE/LDECrescente.java
+++ LDE/LDECrescente.java
@@ -1,6 +1,8 @@
 package LDE;
 //Lista Duplamente Encadeada Ordenada Em Ordem Crescente e Sem Repetições
 
+import java.security.cert.LDAPCertStoreParameters;
+
 public class LDECrescente<T extends Comparable<T>> {
     private LDENode<T> prim;
     private LDENode<T> ult;
@@ -11,7 +13,7 @@ public class LDECrescente<T extends Comparable<T>> {
         this.limite = tam;
     }
 
-    public boolean isEmpty() { //Verifica se a lista tá vazia
+    public boolean isEmpty() { // Verifica se a lista tá vazia
         if (this.qtd == 0) {
             return true;
         } else {
@@ -19,26 +21,26 @@ public class LDECrescente<T extends Comparable<T>> {
         }
     }
 
-    public void inserir(T valor) { //Insere um novo nó na lista
+    public void inserir(T valor) { // Insere um novo nó na lista
         LDENode<T> novo = new LDENode(valor);
         LDENode<T> aux, anterior;
-        if (this.isEmpty() == true) { //Lista vazia?
+        if (this.isEmpty() == true) { // Lista vazia?
             this.prim = novo;
             this.ult = novo;
             this.qtd++;
-        } else if (valor.compareTo(this.prim.getInfo()) < 0 ){ // Lista com um nó. Insere antes do primeiro
+        } else if (valor.compareTo(this.prim.getInfo()) < 0) { // Lista com um nó. Insere antes do primeiro
             novo.setProx(this.prim);
             this.prim.setAnt(novo);
             this.prim = novo;
             this.qtd++;
-        } else if (valor.compareTo(this.prim.getInfo()) == 0 ){ // Verifica repetição
+        } else if (valor.compareTo(this.prim.getInfo()) == 0) { // Verifica repetição
             System.out.println("Valor Repetido. Inserção não efetuada.");
         } else if (valor.compareTo(this.ult.getInfo()) > 0) { // Lista com mais de um nó. Insere depois do último
             this.ult.setProx(novo);
             novo.setAnt(this.ult);
             this.ult = novo;
             this.qtd++;
-        } else if (valor.compareTo(this.ult.getInfo()) == 0 ){ // Verifica repetição
+        } else if (valor.compareTo(this.ult.getInfo()) == 0) { // Verifica repetição
             System.out.println("Valor Repetido. Inserção não efetuada.");
         } else { // Lista com mais de um nó. Insere no meio
             aux = this.prim.getProx();
@@ -46,7 +48,7 @@ public class LDECrescente<T extends Comparable<T>> {
                 if (valor.compareTo(aux.getInfo()) == 0) { // Verifica repetição
                     System.out.println("Valor repetido. Inserção não efetuada.");
                     break;
-                } else if (valor.compareTo(aux.getInfo()) < 0){ // Insere no meio da lista
+                } else if (valor.compareTo(aux.getInfo()) < 0) { // Insere no meio da lista
                     anterior = aux.getAnt();
                     anterior.setProx(novo);
                     aux.setAnt(novo);
@@ -62,7 +64,7 @@ public class LDECrescente<T extends Comparable<T>> {
         }
     }
 
-    private LDENode<T> buscar (T valor) { // Busca o nó na lista
+    private LDENode<T> buscar(T valor) { // Busca o nó na lista
         LDENode<T> aux;
         if (this.isEmpty() == true) {
             return null;
@@ -79,56 +81,33 @@ public class LDECrescente<T extends Comparable<T>> {
         }
     }
 
-    public void remover (T valor) { // Remove um valor específico
-        LDENode<T> aux, anterior, atual;
-        if (this.isEmpty() == true) {
-            System.out.println("Lista vazia!");
+    public void remover(T valor) { // Remove um valor específico
+        LDENode<T> retorno = this.buscar(valor);
+        LDENode<T> anterior, proximo;
+        if (retorno == null) {
+            System.out.println("valor não encontrado!");
         } else if (this.qtd == 1) {
-            if (valor.compareTo(this.prim.getInfo()) == 0) {
-                this.prim = null;
-                this.ult = null;
-                this.qtd = 0;
-            } else {
-                System.out.println("Valor não pertence a lista");
-            }
-        } else { // lista com mais de um nó
-            if (valor.compareTo(this.prim.getInfo()) == 0) { // Remove o primeiro
-                this.prim = this.prim.getProx();
-                this.qtd--;
-            } else if (valor.compareTo(this.prim.getInfo()) < 0) {
-                System.out.println("Valor não pertence a lista");
-            } else if (valor.compareTo(this.ult.getInfo()) == 0) { // Remove o último
-                aux = this.prim;
-                while (aux.getProx() != this.ult) {
-                    aux = aux.getProx();
-                }
-                aux.setProx(null);
-                this.ult = aux;
-                this.qtd--;
-            } else if (valor.compareTo(this.ult.getInfo()) > 0) {
-                System.out.println("Valor não pertence a lista");
-            } else { // remoção no "meio da lista"
-                anterior = null;
-                atual = this.prim;
-                while (atual != null) {
-                    if (valor.compareTo(atual.getInfo()) == 0) { // Quando achar, remove
-                        anterior.setProx(atual.getProx());
-                        this.qtd--;
-                        break;
-                    } else if (valor.compareTo(atual.getInfo()) < 0) {
-                        System.out.println("Valor não pertence a lista");
-                        break;
-                    } else {
-                        anterior = atual;
-                        atual = atual.getProx();
-                    }
-                }
-            }
+            this.prim = null;
+            this.ult = null;
+            this.qtd = 0;
+        } else if (retorno == this.prim) { // remove o primeiro
+            this.prim = this.prim.getProx();
+            this.prim.setAnt(null);
+            this.qtd--;
+        } else if (retorno == this.ult) { // remove o ultimo
+            this.ult = this.ult.getAnt();
+            this.ult.setProx(null);
+            this.qtd--;
+        } else { // remove no "meio"
+            anterior = retorno.getAnt();
+            proximo = retorno.getProx();
+            anterior.setProx(proximo);
+            proximo.setAnt(anterior);
+            this.qtd--;
         }
-    
     }
 
-    public void exibirTodos () { // Exibe todos os valores da lista
+    public void exibirTodos() { // Exibe todos os valores da lista
         LDENode<T> aux;
         if (this.isEmpty() == true) {
             System.out.println("Lista Vazia!");
