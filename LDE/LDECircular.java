81edd6302d3ba505a8a32ccfe0e7572a2018fcfa
Caio Vilela <caio.2020200519@unicap.br>
2022-11-05 20:53:56 -0300

    commit

--- LDE/LDECircular.java
+++ LDE/LDECircular.java
@@ -31,6 +31,24 @@ public class LDECircular <T extends Comparable<T>> {
         }
     }
 
+    public void inserirNoFinal (T valor) {
+        LDENode<T> novo = new LDENode(valor);
+        if (this.isEmpty() == true) {
+            this.prim = novo;
+            this.ult = novo;
+            this.qtd++;
+            this.prim.setAnt(this.ult);
+            this.ult.setProx(this.prim);
+        } else {
+            novo.setAnt(this.ult);
+            this.ult.setProx(novo);
+            this.ult = novo;
+            this.qtd++;
+            this.prim.setAnt(this.ult);
+            this.ult.setProx(this.prim);
+        }
+    }
+
     public void exibirTodos() {
         LDENode<T> aux;
         if (this.isEmpty() == true) {
@@ -43,5 +61,80 @@ public class LDECircular <T extends Comparable<T>> {
             } while (aux != this.prim);
         }
         System.out.println(" ");
+    }
+
+    private LDENode<T> buscar (T valor) { // Busca o nó na lista
+        LDENode<T> aux;
+        if (this.isEmpty() == true) {
+            return null;
+        } else {
+            aux = this.prim;
+            while (aux != null) {
+                if (aux.getInfo().compareTo(valor) == 0) {
+                    return aux;
+                } else {
+                    aux = aux.getProx();
+                }
+            }
+            return null;
+        }
+    }
+
+    public void removerPrimeiro(T valor) {
+        if (this.isEmpty() == true) {
+            System.out.println("Lista Vazia!");
+        } else if (this.qtd == 1) {
+            this.prim = null;
+            this.ult = null;
+            this.qtd = 0;
+        } else {
+            this.prim = this.prim.getProx();
+            this.qtd--;
+            this.prim.setAnt(this.ult);
+            this.ult.setProx(this.prim);
+        }
+    }
+
+    public void removerUltimo(T valor) {
+        if (this.isEmpty() == true) {
+            System.out.println("Lista Vazia!");
+        } else if (this.qtd == 1) {
+            this.prim = null;
+            this.ult = null;
+            this.qtd = 0;
+        } else {
+            this.ult = this.ult.getAnt();
+            this.qtd--;
+            this.prim.setAnt(this.ult);
+            this.ult.setProx(this.prim);
+        }
+    }
+
+    public void remover(T valor) { // Remove um valor específico
+        LDENode<T> retorno = this.buscar(valor);
+        LDENode<T> anterior, proximo;
+        if (retorno == null) {
+            System.out.println("valor não encontrado!");
+        } else if (this.qtd == 1) {
+            this.prim = null;
+            this.ult = null;
+            this.qtd = 0;
+        } else if (retorno == this.prim) { // remove o primeiro
+            this.prim = this.prim.getProx();
+            this.qtd--;
+            this.prim.setAnt(this.ult);
+            this.ult.setProx(this.prim);
+        } else if (retorno == this.ult) { // remove o ultimo
+            this.ult = this.ult.getAnt();
+            this.qtd--;
+            this.prim.setAnt(this.ult);
+            this.ult.setProx(this.prim);
+        } else { // remove no "meio"
+            anterior = retorno.getAnt();
+            proximo = retorno.getProx();
+            anterior.setProx(proximo);
+            proximo.setAnt(anterior);
+            this.qtd--;
+        }
     }
 }
--- LDE/TesteLDECircular.java
+++ LDE/TesteLDECircular.java
@@ -20,13 +20,18 @@ public class TesteLDECircular {
                     lista.inserirNoInicio(val);
                     break;
                 case 2:
-                    lista.exibirTodos();
+                    System.out.print("Informe o valor a ser inserido: ");
+                    val = in.nextInt();
+                    lista.inserirNoFinal(val);
                     break;
                 case 3:
-                    
+                    lista.exibirTodos();
                     break;
                 case 4:
-                    
+                    lista.exibirTodos();
+                    System.out.print("Informe o valor a ser removido: ");
+                    val = in.nextInt();
+                    lista.remover(val);
                     break;
                 case 0:
                     System.out.println("Programa Encerrado!");
@@ -39,10 +44,10 @@ public class TesteLDECircular {
 
     public static void exibeMenu() {
         System.out.println("Opções");
-        System.out.println("1 - Insirir valor na lista");
-        System.out.println("2 - Remover valor da lista");
-        System.out.println("3 - Exibir todos os valores da lista");
-        System.out.println("4 - Buscar um valor na lista");
+        System.out.println("1 - Inserir valor no início");
+        System.out.println("2 - Inserir valor no final");
+        System.out.println("3 - Exibir todos os valores");
+        System.out.println("4 - Remover valor da lista");
         System.out.println("0 - Encerrar programa");
         System.out.print("Digite a opção desejada: ");
     }
