112eab9c87c529435b27e7adf77e631d120e2a7f
Caio Vilela <caio.2020200519@unicap.br>
2022-11-07 09:54:14 -0300

    alt teste

--- LDE/TesteLDECircular.java
+++ LDE/TesteLDECircular.java
@@ -14,24 +14,37 @@ public class TesteLDECircular {
             op = in.nextInt();
             in.nextLine();
             switch (op) {
-                case 1:
+                case 1: // inserir primeiro
                     System.out.print("Informe o valor a ser inserido: ");
                     val = in.nextInt();
                     lista.inserirNoInicio(val);
                     break;
-                case 2:
+                case 2: // inserir último
                     System.out.print("Informe o valor a ser inserido: ");
                     val = in.nextInt();
                     lista.inserirNoFinal(val);
                     break;
-                case 3:
+                case 3: // exibir todos
                     lista.exibirTodos();
                     break;
-                case 4:
+                case 4: // exibir inverso
+                    lista.exibirInverso();
+                    break;
+                case 5: // remover valor especíico
                     lista.exibirTodos();
                     System.out.print("Informe o valor a ser removido: ");
                     val = in.nextInt();
                     lista.remover(val);
+                    break;
+                case 6: // remover primeiro
+                    System.out.print("Informe o valor a ser inserido: ");
+                    val = in.nextInt();
+                    lista.removerPrimeiro(val);
+                    break;
+                case 7: // remover último
+                    System.out.print("Informe o valor a ser inserido: ");
+                    val = in.nextInt();
+                    lista.removerUltimo(val);
                     break;
                 case 0:
                     System.out.println("Programa Encerrado!");
@@ -47,7 +60,10 @@ public class TesteLDECircular {
         System.out.println("1 - Inserir valor no início");
         System.out.println("2 - Inserir valor no final");
         System.out.println("3 - Exibir todos os valores");
-        System.out.println("4 - Remover valor da lista");
+        System.out.println("4 - Exibir todos os valores inversamente");
+        System.out.println("5 - Remover valor específico da lista");
+        System.out.println("6 - Remover o primeiro valor");
+        System.out.println("7 - Remover o último valor");
         System.out.println("0 - Encerrar programa");
         System.out.print("Digite a opção desejada: ");
     }
